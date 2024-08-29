6f234dacde3842a0296d90de6d4033709294af4e
Vinicius Fidelis <160151701+fidelissss@users.noreply.github.com>
2024-08-29 12:14:41 -0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function validarFormulario() {
-var nome = document.getElementById("nome"). value;
-var mensagemErro = document.getElementById("mensagemErro");
-
-if (nome ==="")
-
-
-    mensagemErro.innerHTML = "Por favor, preencha o campo de nome";
-    return false;
+    var nome = document.getElementById("nome"). value;
+    var mensagemErro = document.getElementById("mensagemErro");
+    
+    if (nome ==="") {
+        mensagemErro.innerHTML = "Por favor, preencha o campo de nome";        
+        return false;
+    }
+    return true;
 
 }
 
